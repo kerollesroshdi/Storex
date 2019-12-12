43bedbe27376d19465fdd37e2405189ed0f461bb
Kerolles Roshdi <kerolles.roshdi@mosandah.com.sa>
2019-12-12 16:52:14 +0200

    customize view initial

---
+++ Storex/Shop/ProductDetails/CustomizeView/CustomizeView.swift
@@ -0,0 +1,27 @@
+//
+//  CustomizeView.swift
+//  Storex
+//
+//  Created by admin on 12/12/19.
+//  Copyright © 2019 KerollesRoshdi. All rights reserved.
+//
+
+import UIKit
+
+class CustomizeView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        
+    }
+
+}
