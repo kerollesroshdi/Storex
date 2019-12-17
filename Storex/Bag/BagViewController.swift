2aa38727b06f818e000976d31bc2557d997dc171
Kerolles Roshdi <kerolles.roshdi@mosandah.com.sa>
2019-12-17 12:51:47 +0200

    Bag initial setup

--- Storex/Bag/BagViewController.swift
+++ Storex/Bag/BagViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import SideMenu
 
 class BagViewController: UIViewController {
-
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setNavigationTitleImage()
+        
+        initView()
+        initVM()
+    }
+    
+    func setNavigationTitleImage() {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "nav-logo")!
+        self.navigationItem.titleView = imageView
+    }
+    
+    func initView() {
+        
+        menuButton.rx.tap
+        .subscribe(onNext: { [weak self] _ in
+            guard let self = self else { return }
+            let menu = self.storyboard?.instantiateViewController(withIdentifier: "LeftSideMenu") as! UISideMenuNavigationController
+            menu.setNavigationBarHidden(true, animated: false)
+            SideMenuManager.default.menuPresentMode = .viewSlideInOut
+            SideMenuManager.default.menuAnimationBackgroundColor = UIColor.clear
+            self.present(menu, animated: true)
+        })
+        .disposed(by: disposeBag)
+        
     }
 
+    func initVM() {
+        
+    }
 
 }
 
